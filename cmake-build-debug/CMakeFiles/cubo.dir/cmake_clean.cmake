dcd0c31ae0bab9588889a54006d6a7a229028ea1
lisbethchavez <chavezlsibeth639@gmail.com>
2021-06-28 14:20:57 -0500

    Ejercicio en clase

---
+++ cmake-build-debug/CMakeFiles/cubo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cubo.dir/main.cpp.obj"
+  "cubo.exe"
+  "cubo.exe.manifest"
+  "cubo.pdb"
+  "libcubo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cubo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
